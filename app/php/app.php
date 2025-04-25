c6e09747a37ae0c967e8b5a8f4c407bfad11bb66
Matteo Bertamini <matteo@bertamini.net>
2025-04-25 19:11:13 +0200

    make log things via logstash from php

--- app/php/app.php
+++ app/php/app.php
@@ -3,7 +3,6 @@
 require __DIR__ . '/vendor/autoload.php';
 
 use Monolog\Logger;
-use Elastica\Client;
 use Monolog\Formatter\JsonFormatter;
 use Monolog\Handler\ErrorLogHandler;
 use Monolog\Handler\RotatingFileHandler;
@@ -15,39 +14,23 @@ $log = new Logger('logger');
 $stdoutHandler = new ErrorLogHandler();
 $formatter = new JsonFormatter();
 $stdoutHandler->setFormatter($formatter);
-$log->pushHandler($stdoutHandler);
 
 // File Handler
 $fileHandler = new RotatingFileHandler(__DIR__.'/logs/app.log', 0, \Monolog\Level::Debug);
 $formatter = new JsonFormatter();
 $fileHandler->setFormatter($formatter);
-$log->pushHandler($fileHandler);
-
-// Elasticsearch Handler
-$elasticaClient = new Client(
-    [
-        'host' => 'elasticsearch',
-        'port' => 9200,
-        'transport' => 'http',
-        'username' => 'elastic',
-        'password' => 'password',
-        'auth' => ["elastic", "password"],
-        'hosts' => ['elasticsearch:9200'],
-    ]
-);
-
-$elasticsearchHandler = new \Monolog\Handler\ElasticaHandler($elasticaClient, ['index' => 'codelytv', 'type' => 'record']);
 
 // Register Handlers
+$log->pushHandler($fileHandler);
 $log->pushHandler($stdoutHandler);
-$log->pushHandler($elasticsearchHandler);
+
 
 // My Application
 $options = getopt('a:b:');
 
 # App Servidor A
 if ($options['a'] === 'warning') {
-    $log->warn('Esto es un Warning', ['Servidor' => 'Servidor A']);
+    $log->warning('Esto es un nuevo Warning', ['Servidor' => 'Servidor A']);
 } else {
     $log->info('Esto es un Info', ['Servidor' => 'Servidor A']);
 }
